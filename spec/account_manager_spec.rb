31a534c090b8513b007f87318f1eea8a51bef1b0
Alastair <alastair.chau@gmail.com>
2022-12-13 12:47:37 +0000

    Initialize account with empty transaction list array

--- spec/account_manager_spec.rb
+++ spec/account_manager_spec.rb
@@ -5,4 +5,9 @@ describe Account_Manager do
   it "initializes with an account balance of zero" do
     expect(described_class.new.balance).to eq(0)
   end
+
+  it "initializes with an empty transactions array" do
+    expect(described_class.new.transactions).to eq([])
+  end
+
 end
