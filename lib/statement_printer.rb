fff6c5647fda3c3884c62cfe1aad68cefc3e9a7f
Alastair <alastair.chau@gmail.com>
2022-12-13 23:55:17 +0000

    Add row formatter to Statement Printer class with helper private methods

--- lib/statement_printer.rb
+++ lib/statement_printer.rb
@@ -1,5 +1,26 @@
+# class handles printing and formatting of the account statement
 class StatementPrinter
+  
   def header
     "date || credit || debit || balance\n"
   end
+
+  def format_rows(transactions)
+    sorted_transactions = transactions.sort_by(&:date).reverse
+
+    sorted_transactions.map do |transaction|
+      "#{transaction.date} || #{transaction_formatter(transaction.credit)}|| #{transaction_formatter(transaction.debit)}|| #{balance_formatter(transaction.balance)}"
+    end.join("\n")
+  end
+
+  private
+
+  def transaction_formatter(credit_or_debit)
+    credit_or_debit ? "#{'%.2f' % credit_or_debit} " : ""
+  end
+
+  def balance_formatter(balance)
+    "#{'%.2f' % balance}"
+  end
+
 end
