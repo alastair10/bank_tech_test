7c438b5f1c4ae2c915e89bf1ec47701b5b329e66
Alastair <alastair.chau@gmail.com>
2022-12-13 20:41:10 +0000

    Added balance property to the Transaction class. Refactored Transaction unit tests to include this new class argument.

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,15 +1,16 @@
 # class stores details for a single transaction
 class Transaction
 
-  attr_reader  :date, :credit, :debit
+  attr_reader  :date, :credit, :debit, :balance
 
-  def initialize(date: nil, credit: nil, debit: nil)
+  def initialize(date: nil, credit: nil, debit: nil, prior_balance:)
 
     param_validator(date, credit, debit)
 
     @date = date
     @credit = credit
     @debit = debit
+    @balance = prior_balance + credit.to_i - debit.to_i
   end
 
   private 
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,40 +1,46 @@
 require "transaction"
 
 describe Transaction do
+
   context "when provided with a credit transaction" do
+    let(:transaction) {Transaction.new(date: "14/01/2023", credit: 500, prior_balance: 100)}
+
     it "has a date property" do
-      transaction = Transaction.new(date: "14/01/2023", credit: 500)
       expect(transaction.date).to eq("14/01/2023")
     end
 
     it "has a credit property" do
-      transaction = Transaction.new(date: "14/01/2023", credit: 500)
       expect(transaction.credit).to eq(500)
+    end
+
+    it "has a balance property that is prior_balance plus credit amount" do
+      expect(transaction.balance).to eq(600)
     end
   end
 
   context "when provided with a debit transaction" do
+    let(:transaction) {Transaction.new(date: "14/01/2023", debit: 900, prior_balance: 1000)}
+  
     it "has a debit property" do
-      transaction = Transaction.new(date: "14/01/2023", debit: 900)
       expect(transaction.debit).to eq(900)
     end
   end
 
   context "when given a credit and debit transaction" do
     it "raises an error" do
-      expect{Transaction.new(date: "14/01/2023", credit: 400, debit: 100)}.to raise_error "The transaction must be either a credit or a debit, not both."
+      expect{Transaction.new(date: "14/01/2023", credit: 400, debit: 100, prior_balance: 500)}.to raise_error "The transaction must be either a credit or a debit, not both."
     end
   end
 
   context "when not given a credit or debit transaction" do
     it "raises an error" do
-      expect{Transaction.new(date: "14/01/2023")}.to raise_error "The transaction must be a credit or a debit."
+      expect{Transaction.new(date: "14/01/2023", prior_balance: 0)}.to raise_error "The transaction must be a credit or a debit."
     end
   end
 
   context "when the transaction is missing a date" do
     it "raises an error" do
-      expect{transaction = Transaction.new(credit: 850)}.to raise_error ("The transaction must have a date.")
+      expect{transaction = Transaction.new(credit: 850, prior_balance: 0)}.to raise_error ("The transaction must have a date.")
     end
   end
 end
